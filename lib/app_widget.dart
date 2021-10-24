c2a47d7561ef440c9e6d05fe46b3f2f043320dbe
rafa160 <rafaelpaz86@gmail.com>
2021-10-24 00:14:01 -0300

    Create flow for App Module.

---
+++ lib/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Git Repositories',
+      debugShowCheckedModeBanner: false,
+      home: SplashModule(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:git_repositories/app_module.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Container(),
-    );
-  }
+  runApp(AppModule());
 }
